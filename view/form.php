63b8215d5b05844abc047848329f46f7212631b6
Ivan lopez <42913512+Ivandariotv@users.noreply.github.com>
2021-05-16 10:33:59 -0500

    Desarrollo en proceso 2

--- view/form.php
+++ view/form.php
@@ -1,17 +1,22 @@
 <section class="main-section">
     <div class="banner">
-        <h1>TU REGISTRO ES</h1>
-        <h1>IMPORTANTE</h1>
-        <h1>PARA NOSOTROS</h1>
-        <h1>Y TU SEGURIDAD</h1>
+        <article>
+            <h1 class="no-margin">TU REGISTRO ES</h1>
+            <h2 class="no-margin">IMPORTANTE</h2>
+            <h3 class="no-margin">PARA NOSOTROS</h3>
+            <h4 class="no-margin">Y TU SEGURIDAD</h4>
+        </article>
     </div>
+
     <div class="form-container">
         <form action="">
-            <img src="" alt="logo">
+            <div>
+                <img src="resource/image/logo.png" alt="logo">
+            </div>
             <p>Para continuar, Registra tu ingreso.</p>
             <input type="text" placeholder="Ingrese su numero de documento">
             <input type="text" placeholder="¿Que actividad va a realizar?">
-            <button>Registrar</button>
+            <input type="button" value="Registrar">
             <a href="#">Terminos y condiciones de uso</a>
         </form>
     </div>
